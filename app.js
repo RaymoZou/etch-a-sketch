0b5485de12328b7005f8e5d9bf44e1781fca51c8
Raymond Zou <raymond.1zou@gmail.com>
2022-07-26 22:39:12 -0700

    add user input for custom grid size

--- app.js
+++ app.js
@@ -7,9 +7,43 @@ function addHoverEffect(e) {
     this.classList.add('hover');
 }
 
-for (let i=0; i<16; i++) {
-    const div = document.createElement('div');
-    div.addEventListener('mouseover', addHoverEffect)
-    container.appendChild(div);
+function createGrid(numRows) {
+
+    deleteChildren();
+
+    if (numRows > 100) return;
+    
+    let gridSize = Math.pow(numRows, 2);
+
+    for (let i=0; i<gridSize; i++) {
+        const div = document.createElement('div');
+        div.addEventListener('mouseover', addHoverEffect)
+        div.style.height = (400/numRows - 3) + "px";
+        div.style.width = (400/numRows - 3) + "px";
+        container.appendChild(div);
+    }
 }
+
+function readInput() {
+    const gridSize = prompt('Enter a grid size from 1-100: ');
+    createGrid(gridSize);
+}
+
+function deleteChildren() {
+    const container = document.querySelector('.container');
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);  
+    }
+}
+
+function clearBoard() {
+    const divs = document.querySelectorAll('.container div');
+    console.log(divs);
+    divs.forEach(div => {
+        div.classList.remove("hover");
+    })
+}
+
+createGrid(4);
+
 
